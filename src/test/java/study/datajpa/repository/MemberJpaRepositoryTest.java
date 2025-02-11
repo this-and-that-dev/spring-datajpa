a072de97976ab13b050fffd1be6a3cf0843f83e2
공수한 <ksh92315@pionnet.co.kr>
2025-02-11 21:01:29 +0900

    일반 jpa로 저장 및 조회

---
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,29 @@
+package study.datajpa.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberJpaRepositoryTest {
+
+    @Autowired
+    MemberJpaRepository memberJpaRepository;
+
+    @Test
+    void testMember() {
+        Member member = new Member("memberA");
+        Member savedMember = memberJpaRepository.save(member);
+        Member findMember = memberJpaRepository.find(savedMember.getId());
+
+        assertEquals(findMember.getId(), savedMember.getId());
+        assertEquals(findMember.getUsername(), savedMember.getUsername());
+        assertEquals(findMember, savedMember);
+    }
+
+}
